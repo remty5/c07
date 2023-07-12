f689ad49a670f38b5c044f7d0c9d82bba42e8481
rvandepu <rvandepu@student.42lehavre.fr>
2023-07-12 20:01:47 +0200

    ex01

---
+++ ex01/ft_range.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvandepu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 19:22:27 by rvandepu          #+#    #+#             */
+/*   Updated: 2023/07/12 19:39:20 by rvandepu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*t;
+
+	if (min >= max)
+		return (NULL);
+	t = malloc((max - min) * sizeof(int));
+	if (t)
+	{
+		while (min < max--)
+			t[max - min] = max;
+	}
+	return (t);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	min;
+	int	max;
+	int	*tab;
+
+	if (argc == 3)
+	{
+		i = 0;
+		min = atoi(argv[1]);
+		max = atoi(argv[2]);
+		tab = ft_range(min, max);
+		while (i < max - min)
+			printf("%d ", tab[i++]);
+		printf("\n");
+	}
+	else
+		printf("Usage: %s <min> <max>\n", argv[0]);
+}
