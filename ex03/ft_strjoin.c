5e796aee7533e019fdf0dace5209c0558facc94e
rvandepu <rvandepu@student.42lehavre.fr>
2023-07-17 15:20:28 +0200

    ex03: this was not how malloc works

--- ex03/ft_strjoin.c
+++ ex03/ft_strjoin.c
@@ -6,12 +6,36 @@
 /*   By: rvandepu <rvandepu@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:24:41 by rvandepu          #+#    #+#             */
-/*   Updated: 2023/07/13 19:27:29 by rvandepu         ###   ########.fr       */
+/*   Updated: 2023/07/17 15:19:53 by rvandepu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+
+int	ft_sbs(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i + 1);
+}
+
+int	ft_ssbs(char **strs, int size)
+{
+	int	i;
+	int	l;
+
+	i = 0;
+	l = 0;
+	while (i < size)
+	{
+		l += ft_sbs(strs[i++]);
+	}
+	return (l);
+}
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
@@ -27,9 +51,8 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 			*str = '\0';
 		return (str);
 	}
-	str = malloc(sizeof(strs) + (sizeof(sep) - 2 * sizeof(char)) * (size - 1));
+	str = malloc(ft_ssbs(strs, size) + (size - 1) * (ft_sbs(sep) - 2) +(k = 0));
 	i = -1;
-	k = 0;
 	while (++i < size)
 	{
 		j = 0;
@@ -39,13 +62,14 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 		while (sep[j] && i < size - 1)
 			str[k++] = sep[j++];
 	}
+	str[k] = '\0';
 	return (str);
 }
 
 /*int	main(int argc, char *argv[])
 {
 	if (argc >= 2)
-		printf("%s\n", ft_strjoin(argc, argv, argv[1]));
+		printf("%s\n", ft_strjoin(argc - 2, argv + 2, argv[1]));
 	else
 		printf("Usage: %s <seperator> [<string> ...]\n", argv[0]);
 }*/
